7e8e090a2ee755275d40c7e26376f4389408e404
Luqman1102 <64735985+Luqman1102@users.noreply.github.com>
2020-09-18 22:36:22 +0700

    Mengubah Template.php menambah bagian footer
    - Sesuai dengan tema Jasain
    - Menambahkan bagian footer
    - Menambahkan link untuk ke javascript sendiri (javascript-custom.js)

--- app/Views/layout/Template.php
+++ app/Views/layout/Template.php
@@ -30,106 +30,8 @@
 
     <?= $this->include('layout/navbar'); ?>
     <?= $this->renderSection('content'); ?>
-
-    <footer>
-
-        <!-- Footer Start-->
-        <div class="footer-main" data-background="assets/img/shape/footer_bg.png">
-            <div class="footer-area footer-padding">
-                <div class="container">
-                    <div class="row d-flex justify-content-between">
-                        <div class="col-lg-3 col-md-4 col-sm-8">
-                            <div class="single-footer-caption mb-50">
-                                <div class="single-footer-caption mb-30">
-                                    <!-- logo -->
-                                    <div class="footer-logo">
-                                        <a href="<?= base_url(); ?>/index.html"><img src="<?= base_url(); ?>/assets/img/logo/logo2_footer.png" alt=""></a>
-                                    </div>
-                                    <div class="footer-tittle">
-                                        <div class="footer-pera">
-                                            <p class="info1">221B Baker Street, P.O Box 353 Park <br> Road, USA - 215431</p>
-                                            <p class="info2">info@yourdomain.com</p>
-                                        </div>
-                                    </div>
-                                    <div class="footer-social">
-                                        <a href="<?= base_url(); ?>/#"><i class="fab fa-facebook-f"></i></a>
-                                        <a href="<?= base_url(); ?>/#"><i class="fab fa-twitter"></i></a>
-                                        <a href="<?= base_url(); ?>/#"><i class="fas fa-globe"></i></a>
-                                        <a href="<?= base_url(); ?>/#"><i class="fab fa-behance"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-2 col-md-4 col-sm-5">
-                            <div class="single-footer-caption mb-50">
-                                <div class="footer-tittle">
-                                    <h4>Quick Links</h4>
-                                    <ul>
-                                        <li><a href="<?= base_url(); ?>/about.html">About</a></li>
-                                        <li><a href="<?= base_url(); ?>/about.html">Features</a></li>
-                                        <li><a href="<?= base_url(); ?>/single-blog.html">Pricing</a></li>
-                                        <li><a href="<?= base_url(); ?>/blog.html">Blog</a></li>
-                                        <li><a href="<?= base_url(); ?>/contact.html">Contact</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-2 col-md-4 col-sm-7">
-                            <div class="single-footer-caption mb-50">
-                                <div class="footer-tittle">
-                                    <h4>Support</h4>
-                                    <ul>
-                                        <li><a href="<?= base_url(); ?>/#">Privacy Policy</a></li>
-                                        <li><a href="<?= base_url(); ?>/#">Terms & Conditions</a></li>
-                                        <li><a href="<?= base_url(); ?>/#"> Sitemap</a></li>
-                                        <li><a href="<?= base_url(); ?>/#">FAQs</a></li>
-                                        <li><a href="<?= base_url(); ?>/#">Report a bugb</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-4 col-sm-5">
-                            <div class="single-footer-caption mb-50">
-                                <div class="footer-tittle">
-                                    <h4>Core Features</h4>
-                                    <ul>
-                                        <li><a href="<?= base_url(); ?>/#">Email Marketing</a></li>
-                                        <li><a href="<?= base_url(); ?>/#"> Offline SEO</a></li>
-                                        <li><a href="<?= base_url(); ?>/#">Social Media Marketing</a></li>
-                                        <li><a href="<?= base_url(); ?>/#">Lead Generation</a></li>
-                                        <li><a href="<?= base_url(); ?>/#"> 24/7 Support</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- footer-bottom aera -->
-            <div class="footer-bottom-area footer-bg">
-                <div class="container">
-                    <div class="footer-border">
-                        <div class="row d-flex align-items-center">
-                            <div class="col-xl-12 ">
-                                <div class="footer-copy-right text-center">
-                                    <p>
-                                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                                        Copyright &copy;<script>
-                                            document.write(new Date().getFullYear());
-                                        </script> All rights reserved | This template is made with <i class="ti-heart" aria-hidden="true"></i> by <a href="<?= base_url(); ?>/https://colorlib.com" target="_blank">Colorlib</a>
-                                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Footer End-->
-
-    </footer>
-
+    <?= $this->include('layout/footer'); ?>
+   
     <!-- JS here -->
 
     <!-- All JS Custom Plugins Link Here here -->
@@ -166,6 +68,9 @@
     <!-- Jquery Plugins, main Jquery -->
     <script src="<?= base_url(); ?>/./assets/js/plugins.js"></script>
     <script src="<?= base_url(); ?>/./assets/js/main.js"></script>
+
+    <!-- JavaScript Custom -->
+    <script src="<?= base_url(); ?>/./assets/js/javascript-custom.js"></script>
 
 </body>
 
