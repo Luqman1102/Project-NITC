34d11fa054372cf7fbff24c6383bfaa1e867c190
Luqman1102 <64735985+Luqman1102@users.noreply.github.com>
2020-09-18 22:38:48 +0700

    Membuat footer.php
    Membagi template menjadi lebih efisien lagi dengan membuat footer

---
+++ app/Views/layout/footer.php
@@ -0,0 +1,88 @@
+<footer>
+
+<!-- Footer Start-->
+<div class="footer-main" data-background="assets/img/shape/footer_bg.png">
+ <div class="footer-area footer-padding">
+     <div class="container">
+         <div class="row d-flex justify-content-between">
+             <div class="col-lg-3 col-md-4 col-sm-8">
+                <div class="single-footer-caption mb-50">
+                  <div class="single-footer-caption mb-30">
+                       <!-- logo -->
+                      <div class="footer-logo">
+                          <a href="index.html"><img src="<?= base_url()?>/assets/img/logo/logojasain.png" alt=""></a>
+                      </div>
+                      <div class="footer-tittle">
+                          <div class="footer-pera">
+                              <p class="info1"> <br> </p>
+                              <p class="info2">info@Jasain.com</p>
+                         </div>
+                      </div>
+                      <div class="footer-social">
+                         <a href="https://id-id.facebook.com"><i class="fab fa-facebook-f"></i></a>
+                         <a href="https://twitter.com/home"><i class="fab fa-twitter"></i></a>
+                         <a href="https://mail.google.com/mail/"><i class="fas fa-envelope"></i></a>
+                         <a href="https://web.whatsapp.com/"><i class="fab fa-whatsapp"></i></a>
+                     </div>
+                  </div>
+                </div>
+             </div>
+             <div class="col-lg-3 col-md-5 col-sm-7">
+                 <div class="single-footer-caption mb-50">
+                     <div class="footer-tittle">
+                         <h4>Tetang kita</h4>
+                         <ul>
+                            <p>Jasain.com menyediakan berbagai macam jasa mulai dari kebutuhan kelas menengah kebawah sampai kebutuhan kelas menengah keatas. Dapatkan fitur menarik di Jasain.com</p>
+                         </ul>
+                     </div>
+                 </div>
+             </div>
+             <div class="col-lg-2 col-md-4 col-sm-5">
+                 <div class="single-footer-caption mb-50">
+                     <div class="footer-tittle">
+                         <h4>Tautan</h4>
+                         <ul>
+                           <li><a href="about.html">Lain-lain</a></li>
+                             <li><a href="#layan" ata-animation="fadeIn scrollUp" data-delay="1s">Layanan</a></li>
+                             <li><a href="services.html">Harga</a></li>
+                             <li><a href="services.html">Kategori</a></li>
+                             
+                      </ul>
+                     </div>
+                 </div>
+             </div>
+             <div class="col-lg-3 col-md-4 col-sm-6">
+                 <div class="single-footer-caption mb-50">
+                     <div class="footer-tittle">
+                         <h4>Kontak</h4>
+                         <ul>
+                             <li><a href="https://web.whatsapp.com/">+62 882-1147-3717</a></i></li>
+                             <li><a href="https://web.whatsapp.com/">+62 857-3682-7475</a></i></li>
+                             <li><a href="https://myaccount.google.com/">auliask104@gmail.com</a></li>
+                      </ul>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
+ <!-- footer-bottom aera -->
+ <div class="footer-bottom-area footer-bg">
+     <div class="container">
+         <div class="footer-border">
+              <div class="row d-flex align-items-center">
+                  <div class="col-xl-12 ">
+                      <div class="footer-copy-right text-center">
+                          <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> Seluruh hak cipta | Dibuat oleh <a href="https://colorlib.com" target="_blank">Jasain.com</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                      </div>
+                  </div>
+              </div>
+         </div>
+     </div>
+ </div>
+</div>
+<!-- Footer End-->
+
+</footer>
