36167d97d4a11aa5256a637d546c9f8b3602424b
Luqman1102 <64735985+Luqman1102@users.noreply.github.com>
2020-09-19 09:55:36 +0700

    Mengganti struktur tabel User dan Pesanan
    Mengganti struktur tabel User dan Pesanan

--- app/Database/Migrations/2020-08-14-083659_Pesanan.php
+++ app/Database/Migrations/2020-08-14-083659_Pesanan.php
@@ -15,38 +15,62 @@ class Pesanan extends Migration
 				'unsigned'       => true,
 				'auto_increment' => true,
 			],
-			'Judul'       => [
+			'judul'       => [
 				'type'           => 'VARCHAR',
 				'constraint'     => '100',
 			],
-			'Username_pembeli'			=> [
+			'username_pembeli'			=> [
 				'type'			 => 'VARCHAR',
 				'constraint'	 => '100',
 			],
-			'Username_penjualan'		=> [
+			'username_penjual'		=> [
 				'type'			=> 'VARCHAR',
 				'constraint'    => '100',
 			],
-			'Tanggal'			=> [
-				'type'			=> 'DATE',
+			'tanggal'			=> [
+				'type'			=> 'TIMESTAMP',
 			],
-			'Status'			=> [
+			'status'			=> [
 				'type'			=> 'VARCHAR',
 				'constraint'	=> '25',
 			],
-			'Harga'				=> [
+			'jumlah_revisi'				=> [
 				'type'			=> 'INT',
 				'constraint'	=> 11
 			],
+			'harga'				=> [
+				'type'			=> 'INT',
+				'constraint'	=> 11
+			],
+			'gambar_asli'		=> [
+				'type'			=> 'VARCHAR',
+				'constraint'	=> '255',
+			],
+			'gambar_watermark'  => [
+				'type'			=> 'VARCHAR',
+				'constraint'	=> '255',
+			],
+			'nama_rekening'  => [
+				'type'			=> 'VARCHAR',
+				'constraint'	=> '20',
+			],
+			'no_rekening'  => [
+				'type'			=> 'VARCHAR',
+				'constraint'	=> '20',
+			],
+			'bukti_pembayaran'  => [
+				'type'			=> 'VARCHAR',
+				'constraint'	=> '255',
+			]
 		]);
 		$this->forge->addPrimaryKey('id');
-		$this->forge->createTable('Pesanan');
+		$this->forge->createTable('pesanan');
 	}
 
 	//--------------------------------------------------------------------
 
 	public function down()
 	{
-		$this->forge->dropTable('Pesanan');
+		$this->forge->dropTable('pesanan');
 	}
 }
---
+++ app/Database/Migrations/2020-08-19-024313_User.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+use function PHPSTORM_META\type;
+
+class User extends Migration
+{
+	public function up()
+	{
+
+		$this->forge->addField([
+			'id'          => [
+				'type'           => 'INT',
+				'constraint'     => 11,
+				'unsigned'       => true,
+				'auto_increment' => true,
+			],
+			'username'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '100',
+			],
+			'email'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '100',
+			],
+			'password'   => [
+				'type'			 => 'VARCHAR',
+				'constraint' 	 => '100',
+			],
+			'nama_depan' => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '100',
+			],
+			'nama_belakang' => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '100',
+			],
+			'deskripsi_diri'		=> [
+				'type'			 => 'Text',
+				'default'		 => null,
+			],
+			'role'				 => [
+				'type'			 => 'ENUM',
+				'constraint'	 => ['penjual', 'pembeli'],
+				'default'		 => null,
+			],
+			'nama_rekening'		 => [
+				'type' 			 => 'VARCHAR',
+				'constraint' 	 => '20',
+			],
+			'no_rekening'		 => [
+				'type' 			 => 'VARCHAR',
+				'constraint' 	 => '20',
+			]
+		]);
+		$this->forge->addPrimaryKey('id');
+		$this->forge->addPrimaryKey('username');
+		$this->forge->addUniqueKey(['email']);
+		$this->forge->createTable('user');
+	}
+
+	//--------------------------------------------------------------------
+
+	public function down()
+	{
+		$this->forge->dropTable('user');
+	}
+}
